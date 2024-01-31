52e1f6fefce61e6eac821829e71172442194879c
jose <j6alopez@gmail.com>
2024-01-31 13:04:33 +0100

    feat: animation stable

--- src/app/escenes/components/escena/escena.component.ts
+++ src/app/escenes/components/escena/escena.component.ts
@@ -2,80 +2,19 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Step } from '../../interfaces/step.interface';
 import { AnimationMetadata, AnimationTriggerMetadata, animate, animation, group, query, state, style, transition, trigger } from '@angular/animations';
 
-
-const left = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
-  group([
-    query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
-      optional: true,
-    }),
-    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
-      optional: true,
-    }),
-  ]),
-];
-
-const animSlider = trigger('animSlider', [
-  state(
-    'open',
-    style({
-      opacity: 1,
-    })
-  ),
-  state(
-    'close',
-    style({
-      opacity: 0,
-    })
-  ),
-  transition(':increment', animate('2s ease-out')),
-  transition(':decrement', animate('2s ease-out'))
-]);
-
-const right = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
-  group([
-    query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
-      optional: true,
-    }),
-    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
-      optional: true,
-    }),
-  ]),
-];
-
-const enterTransition = transition(':enter', [
-  style({
-    opacity: 0,
-  }),
-  animate('1s ease-in', style({ opacity: 1}))
-])
-
-const exitTransition = transition(':leave', [
-  style( {
-    opacity: 1,
-  }),
-  animate('1s ease-in', style({opacity: 0}))
-])
-
-const fadeIn = trigger('fadeIn', [enterTransition]);
-const fadeOut = trigger('fadeOut', [exitTransition]);
-
-
-
 @Component({
   selector: 'escenes-escena',
   templateUrl: './escena.component.html',
   styleUrl: './escena.component.scss',
   animations: [
-    trigger('fadeAnimation', [
+    trigger('sliderAnimation', [
       transition(':increment', [
         style({ transform: 'translateX(0%)', opacity: 1 }),
-        animate('500ms', style({ transform: 'translateX(-100%)', opacity: 0})),
+        animate('500ms ease-out', style({ transform: 'translateX(-100%)', opacity: 0})),
       ]),
       transition(':decrement', [
         style({ transform: 'translateX(-100%)', opacity: 0 }),
-        animate('500ms', style({ transform: 'translateX(0%)', opacity: 1 })),
+        animate('500ms ease-in', style({ transform: 'translateX(0%)', opacity: 1 })),
       ]),
     ]),
   ],
